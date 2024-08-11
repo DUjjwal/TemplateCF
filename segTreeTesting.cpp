fc47c774961fc573f823f6557a8fa3bc03fc2d6d
Ujjwal Dhiman <137422120+DUjjwal@users.noreply.github.com>
2024-08-11 19:18:33 +0530

    Update segTreeTesting.cpp

--- segTreeTesting.cpp
+++ segTreeTesting.cpp
@@ -1,70 +1,85 @@
-struct item {
-    int sum;
-};
-struct segtree {
-    int size;
-    vector <item> values;
-
-    item NEUTRAL_ELEMENT={0}; //CHANGE THIS
-
-    item merge(item a,item b) {
-        return {a.sum+b.sum}; //CHANGE THIS
+#define ll long long
+template<typename Node, typename Update>
+struct SegTree {
+    vector<Node> tree;
+    vector<ll> arr; // type may change
+    int n;
+    int s;
+    SegTree(int a_len, vector<ll> &a) { // change if type updated
+        arr = a;
+        n = a_len;
+        s = 1;
+        while(s < 2 * n){
+            s = s << 1;
+        }
+        tree.resize(s); fill(tree.begin(),tree.end(), Node());
+        build(0, n - 1, 1);
     }
-
-    item single(int v) {
-        return {v}; //CHANGE THIS
-    }
-
-    segtree(vector <int> &temp) {
-        init(temp.size());
-        build(temp);
-    }
-    void init(int n) {
-        size=1;
-        while(size<n)size*=2;
-        values.resize(2*size);
-    }
-
-    void build(vector <int> &a,int x,int lx,int rx) {
-        if(lx==rx) {
-            if(lx<(int)a.size())
-            values[x]=single(a[lx]);
+    void build(int start, int end, int index)  // Never change this
+    {
+        if (start == end)   {
+            tree[index] = Node(arr[start]);
             return;
         }
-        int mid=(lx+rx)/2;
-        build(a,2*x+1,lx,mid);
-        build(a,2*x+2,mid+1,rx);
-        values[x]=merge(values[2*x+1],values[2*x+2]);
+        int mid = (start + end) / 2;
+        build(start, mid, 2 * index);
+        build(mid + 1, end, 2 * index + 1);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
     }
-    void build(vector <int> &a) {
-        build(a,0,0,size-1);
-    }
-    void set(int i,int v,int x,int lx,int rx) {
-        if(lx==rx) {
-            values[x]=single(v);
+    void update(int start, int end, int index, int query_index, Update &u)  // Never Change this
+    {
+
+        if (start == end) {
+            u.apply(tree[index]);
             return;
         }
-        int mid=(lx+rx)/2;
-        if(i<=mid)
-        set(i,v,2*x+1,lx,mid);
+        int mid = (start + end) / 2;
+        if (mid >= query_index)
+            update(start, mid, 2 * index, query_index, u);
         else
-        set(i,v,2*x+2,mid+1,rx);
+            update(mid + 1, end, 2 * index + 1, query_index, u);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+    }
+    Node query(int start, int end, int index, int left, int right) { // Never change this
+        if (start > right || end < left)
+            return Node();
+        if (start >= left && end <= right)
+            return tree[index];
+        int mid = (start + end) / 2;
+        Node l, r, ans;
+        l = query(start, mid, 2 * index, left, right);
+        r = query(mid + 1, end, 2 * index + 1, left, right);
+        ans.merge(l, r);
+        return ans;
+    }
+    void make_update(int index, ll val) {  // pass in as many parameters as required
+        Update new_update = Update(val); // may change
+        update(0, n - 1, 1, index, new_update);
+    }
+    Node make_query(int left, int right) {
+        return query(0, n - 1, 1, left, right);
+    }
+};
 
-        values[x]=merge(values[2*x+1],values[2*x+2]);
-    } 
-    void set(int i,int v) {
-        set(i,v,0,0,size-1);
+struct Node1 {
+    ll val; // may change
+    Node1() { // Identity element
+        val = 0;    // may change
     }
-    int calc(int x,int lx,int rx,int l,int r) { //CHANGE THIS
-        if(lx>r || l>rx)return 0;
-        else if(lx>=l && rx<=r)return values[x].sum;
-        int mid=(lx+rx)/2;
-        int a=calc(2*x+1,lx,mid,l,r);
-        int b=calc(2*x+2,mid+1,rx,l,r);
-        return a+b;
+    Node1(ll p1) {  // Actual Node
+        val = p1; // may change
     }
-    int calc(int l,int r) { //CHANGE THIS
-        return calc(0,0,size-1,l,r);
+    void merge(Node1 &l, Node1 &r) { // Merge two child nodes
+        val = l.val ^ r.val;  // may change
     }
-        
+};
+
+struct Update1 {
+    ll val; // may change
+    Update1(ll p1) { // Actual Update
+        val = p1; // may change
+    }
+    void apply(Node1 &a) { // apply update to given node
+        a.val = val; // may change
+    }
 };
