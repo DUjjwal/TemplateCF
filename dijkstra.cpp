2d86fcef53b0101fce6fdaf64fb62cbc46fb9051
Ujjwal Dhiman <137422120+DUjjwal@users.noreply.github.com>
2024-08-25 22:39:44 +0530

    Add files via upload

---
+++ dijkstra.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define pp pair<int,int>
+using namespace std;
+vector <list<pp>> graph;
+int dijkstra(int src,int n)
+{
+    priority_queue<pp,vector <pp>,greater<pp>> pq;
+    vector <bool> vis(n,false);
+    map <int,int> dist,via;
+    for(int i=0;i<n;i++)dist[i]=INT_MAX;
+    pq.push({0,src});//distance - node
+    dist[src]=0;
+    while(pq.size()) {
+        pp curr=pq.top();
+        if(vis[curr.second]) {
+            pq.pop();
+            continue;
+        }
+        vis[curr.second]=true;
+        pq.pop();
+        for(auto neigh:graph[curr.second]) {
+            if(!vis[neigh.first] && neigh.second+dist[curr.second] < dist[neigh.first]) {
+                pq.push({neigh.second+dist[curr.second],neigh.first});
+                dist[neigh.first]=neigh.second+dist[curr.second];
+                via[neigh.first]=curr.second;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    cout<<dist[i]<<" ";
+}
+int main()
+{
+    int n,m;cin>>n>>m;
+    graph.resize(n,list <pp> ());
+    while(m--) {
+        int a,b,c;cin>>a>>b>>c;
+        graph[a].push_back({b,c});
+        graph[b].push_back({a,c});
+    }
+    dijkstra(0,n);
+}
